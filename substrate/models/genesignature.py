d3e314e288b54015815b3c0178ee3dc8fa6bc7c5
Greg <ggundersen@gmail.com>
2015-12-10 17:54:57 -0500

    Switching from gene_lists to gene_list

--- substrate/models/genesignature.py
+++ substrate/models/genesignature.py
@@ -2,7 +2,6 @@
 processed data, with links to the SOFT file, gene lists, and metadata.
 """
 
-
 import hashlib
 import time
 
@@ -17,12 +16,27 @@ class GeneSignature(db.Model):
     # is how the front-end identifies the dataset, so that we do not display
     # the actual database ID to the users.
     extraction_id = db.Column(db.String(10))
-    soft_file = db.relationship('SoftFile', uselist=False, backref='gene_signatures')
-    gene_lists = db.relationship('GeneList', backref=db.backref('gene_signature', order_by=id))
-    required_metadata = db.relationship('RequiredMetadata', uselist=False, backref=db.backref('gene_signature', order_by=id))
-    optional_metadata = db.relationship('OptionalMetadata', backref=db.backref('gene_signature', order_by=id))
+    soft_file = db.relationship(
+        'SoftFile',
+        uselist=False,
+        backref='gene_signatures'
+    )
+    gene_list = db.relationship(
+        'GeneList',
+        uselist=False,
+        backref=db.backref('gene_signature', order_by=id)
+    )
+    required_metadata = db.relationship(
+        'RequiredMetadata',
+        uselist=False,
+        backref=db.backref('gene_signature', order_by=id)
+    )
+    optional_metadata = db.relationship(
+        'OptionalMetadata',
+        backref=db.backref('gene_signature', order_by=id)
+    )
 
-    def __init__(self, soft_file, gene_lists, required_metadata, optional_metadata, tags):
+    def __init__(self, soft_file, gene_list, required_metadata, optional_metadata, tags):
         """Construct an Extraction instance. This is called only by class
         methods.
         """
@@ -30,7 +44,7 @@ class GeneSignature(db.Model):
         # simply guess the ID for other user's data.
         self.extraction_id = hashlib.sha1(str(time.time())).hexdigest()[:10]
         self.soft_file = soft_file
-        self.gene_lists = gene_lists
+        self.gene_list = gene_list
         self.required_metadata = required_metadata
         self.optional_metadata = optional_metadata
         self.tags = tags
@@ -43,7 +57,7 @@ class GeneSignature(db.Model):
         return {
             'extraction_id': self.extraction_id,
             'soft_file': self.soft_file.serialize,
-            'gene_lists': [gl.serialize for gl in self.gene_lists],
+            'gene_lists': self.gene_list.serialize,
             'required_metadata': self.required_metadata.serialize,
             'optional_metadata': {om.name: om.value for om in self.optional_metadata},
             'tags': [t.name for t in self.tags]
