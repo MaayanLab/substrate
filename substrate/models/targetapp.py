0b3ebb7bb8e5c64d8c400c891123a21aa62dde30
Greg <ggundersen@gmail.com>
2015-12-15 15:48:47 -0500

    Added target app description

--- substrate/models/targetapp.py
+++ substrate/models/targetapp.py
@@ -9,6 +9,7 @@ class TargetApp(db.Model):
     __tablename__ = 'target_app'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), unique=True, nullable=False)
+    description = db.Column(db.String(1024), nullable=False)
     target_app_links = db.relationship(
         'TargetAppLink',
         backref=db.backref('target_app', order_by=id)
