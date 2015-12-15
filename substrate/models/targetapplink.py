5ec5bbc4d7c664ae4b2e5fad9c5b009139ec2c42
Greg <ggundersen@gmail.com>
2015-12-15 15:07:53 -0500

    Added gene_list_direction column

--- substrate/models/targetapplink.py
+++ substrate/models/targetapplink.py
@@ -11,10 +11,12 @@ class TargetAppLink(db.Model):
     target_app_fk = db.Column(db.Integer, db.ForeignKey('target_app.id'))
     gene_list_fk = db.Column(db.Integer, db.ForeignKey('gene_list.id'))
     link = db.Column(db.Text)
+    gene_list_direction = db.Column(db.Integer)
 
-    def __init__(self, target_app, link):
+    def __init__(self, target_app, gene_list_direction, link):
         self.target_app = target_app
         self.link = link
+        self.gene_list_direction = gene_list_direction
 
     def __repr__(self):
         return '<TargetAppLink %r>' % self.id
