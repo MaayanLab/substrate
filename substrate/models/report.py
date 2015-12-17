14dc5068e6674a3c53d4ff1de13207d15ba4eed4
Greg <ggundersen@gmail.com>
2015-12-17 18:07:26 -0500

    Changed status to ready boolean

--- substrate/models/report.py
+++ substrate/models/report.py
@@ -8,7 +8,7 @@ class Report(db.Model):
 
     __tablename__ = 'report'
     id = db.Column(db.Integer, primary_key=True)
-    status = db.Column(db.String(32), nullable=False)
+    ready = db.Column(db.Boolean, nullable=False)
     report_type = db.Column(db.Text, nullable=False)
     tag_fk = db.Column(db.Integer, db.ForeignKey('tag.id'))
 
@@ -23,6 +23,3 @@ class Report(db.Model):
 
     def __repr__(self):
         return '<Report %r>' % self.id
-
-    def ready(self):
-        self.status = 'ready'
