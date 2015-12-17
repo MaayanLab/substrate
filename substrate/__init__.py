8e00343e3d60576e1103da9b5a05cc024bb07469
Greg <ggundersen@gmail.com>
2015-12-17 10:26:43 -0500

    Removed unnecessary aliasing

--- substrate/__init__.py
+++ substrate/__init__.py
@@ -6,17 +6,17 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-from models.customdataset import CustomDataset as CustomDataset
-from models.dataset import Dataset as Dataset
-from models.gene import Gene as Gene
-from models.genelist import GeneList as GeneList
-from models.genesignature import GeneSignature as GeneSignature
-from models.geodataset import GeoDataset as GeoDataset
-from models.optionalmetadata import OptionalMetadata as OptionalMetadata
-from models.rankedgene import RankedGene as RankedGene
-from models.requiredmetadata import RequiredMetadata as RequiredMetadata
-from models.softfile import SoftFile as SoftFile
-from models.softfilesample import SoftFileSample as SoftFileSample
+from models.customdataset import CustomDataset
+from models.dataset import Dataset
+from models.gene import Gene
+from models.genelist import GeneList
+from models.genesignature import GeneSignature
+from models.geodataset import GeoDataset
+from models.optionalmetadata import OptionalMetadata
+from models.rankedgene import RankedGene
+from models.requiredmetadata import RequiredMetadata
+from models.softfile import SoftFile
+from models.softfilesample import SoftFileSample
 from models.tag import Tag as Tag
-from models.targetapp import TargetApp as TargetApp
-from models.targetapplink import TargetAppLink as TargetAppLink
+from models.targetapp import TargetApp
+from models.targetapplink import TargetAppLink
