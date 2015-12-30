a19d95493ef9367ebb0b4a30170fcbab8fa23377
Greg <ggundersen@gmail.com>
2015-12-30 17:05:39 -0500

    Adding additional viz_type--gen3va

--- substrate/models/hierclustvisualization.py
+++ substrate/models/hierclustvisualization.py
@@ -18,8 +18,8 @@ class HierClustVisualization(db.Model):
 
     def __init__(self, link, viz_type, target_app, enrichr_library=None):
         self.link = link
-        if viz_type not in ['enrichr', 'l1000cds2']:
-            raise ValueError('viz_type must be "enrichr" or "l1000cds2"')
+        if viz_type not in ['enrichr', 'l1000cds2', 'gen3va']:
+            raise ValueError('viz_type must be "enrichr", "l1000cds2", or "gen3va"')
         self.viz_type = viz_type
         self.target_app = target_app
         self.enrichr_library = enrichr_library
