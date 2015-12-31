5d707ff63733bd4095fdf1d92199ef5d300e7689
Greg <ggundersen@gmail.com>
2015-12-31 12:00:27 -0500

    reset() function for consistent reseting of reports

--- substrate/models/report.py
+++ substrate/models/report.py
@@ -1,11 +1,13 @@
 """Parent class for GEN3VA reports.
 """
 
-from substrate import db
+from substrate import db, HierClustVisualization, PCAVisualization
+
+PROCESSING = 'processing'
+READY = 'ready'
 
 
 class Report(db.Model):
-
     __tablename__ = 'report'
     id = db.Column(db.Integer, primary_key=True)
     status = db.Column(db.String, nullable=False)
@@ -24,10 +26,10 @@ class Report(db.Model):
     )
 
     # TODO: Implement custom vs tag-based reporting.
-    #__mapper_args__ = {'polymorphic_on': report_type}
+    # __mapper_args__ = {'polymorphic_on': report_type}
 
     def __init__(self, report_type, tag):
-        self.status = 'processing'
+        self.status = PROCESSING
         self.report_type = report_type
         self.tag = tag
         self.hier_clusts = []
@@ -41,10 +43,24 @@ class Report(db.Model):
     def set_pca_visualization(self, pca_visualization):
         self.pca_visualization = pca_visualization
 
-    @property
-    def ready(self):
-        return self.status == 'ready'
+    def reset(self):
+        for hier_clust in self.hier_clusts:
+            HierClustVisualization \
+                .query \
+                .filter_by(id=hier_clust.id) \
+                .delete()
+
+        if self.pca_visualization:
+            PCAVisualization \
+                .query \
+                .filter_by(id=self.pca_visualization.id) \
+                .delete()
+        self.status = PROCESSING
 
     @property
-    def pending(self):
-        return self.status == 'pending'
+    def ready(self):
+        return self.status == READY
+
+    @property
+    def processing(self):
+        return self.status == PROCESSING
